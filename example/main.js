5b40075a0e28162ba51097b010375b548b5f7340
pdsuwwz <pdsu.wwz@foxmail.com>
2022-01-02 12:29:13 +0800

    🐛 fix: dev example lib

--- example/main.js
+++ example/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import router from './router.js'
 import App from './App.vue'
 
-import HocElementAffix from 'root/lib/hoc-el-affix' // Switch to bundle lib
-// import HocElementAffix from '@/main.js'
+// import HocElementAffix from 'root/lib/hoc-el-affix' // Switch to bundle lib
+import HocElementAffix from '@/main.js'
 
 import 'example/styles/variables.scss'
 
